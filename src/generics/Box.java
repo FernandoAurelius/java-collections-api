cf1e5a77a12aaf6c7853c95b932456af4b3ea6d5
Miguel Fernando Flores <miguelfernandoaurelius@gmail.com>
2024-09-25 19:07:27 -0300

    Create basic class Box to show use of Generics Type in Java

---
+++ src/generics/Box.java
@@ -0,0 +1,13 @@
+package generics;
+
+public class Box {
+    private Object object;
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+}
