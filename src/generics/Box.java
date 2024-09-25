ff8a1e6db96a85977aaeef3e916c9f047a5fcfa1
Miguel Fernando Flores <miguelfernandoaurelius@gmail.com>
2024-09-25 19:13:30 -0300

    refactor(generics/Box.java): change class to use generics
    This commit refactors the Box class to use generics, allowing it to hold any type of object. This improves the type safety of the class and makes it more flexible.

--- src/generics/Box.java
+++ src/generics/Box.java
@@ -1,13 +1,13 @@
 package generics;
 
-public class Box {
-    private Object object;
+public class Box<T> {
+    private T t;
 
-    public Object getObject() {
-        return object;
+    public T getT() {
+        return t;
     }
 
-    public void setObject(Object object) {
-        this.object = object;
+    public void setT(T t) {
+        this.t = t;
     }
 }
