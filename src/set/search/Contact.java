9d8a78667545260ae9b0ca5498d4c44b7b633a37
FernandoAurelius <miguelfernandoaurelius@gmail.com>
2024-10-23 16:35:41 -0300

    feat(set/search): add ContactAgenda class with methods to add, show, search and update contacts
    feat(set/search): add Contact class with name and phone number attributes and methods to get and set them
    
    feat(set/search): add main method to ContactAgenda class to test the functionality
    
    feat(set/search): add equals and hashCode methods to Contact class to ensure proper object comparison
    
    feat(set/search): add toString method to Contact class to provide a string representation of the object

---
+++ src/set/search/Contact.java
@@ -0,0 +1,46 @@
+package set.search;
+
+import java.util.Objects;
+
+public class Contact {
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Contact) obj;
+        return Objects.equals(this.name, that.name) &&
+                Objects.equals(this.phoneNumber, that.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, phoneNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Contact[" +
+                "name=" + name + ", " +
+                "phoneNumber=" + phoneNumber + ']';
+    }
+}
---
+++ src/set/search/ContactAgenda.java
@@ -0,0 +1,55 @@
+package set.search;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ContactAgenda {
+
+    @SuppressWarnings("FieldMayBeFinal")
+    private Set<Contact> contacts;
+
+    public ContactAgenda() {
+        this.contacts = new HashSet<>();
+    }
+
+    public void addContact(String name, String phoneNumber) {
+        contacts.add(new Contact(name, phoneNumber));
+    }
+
+    public void showContacts() {
+        contacts.forEach(c -> System.out.println("Name: " + c.getName() + " | Phone: " + c.getPhoneNumber()));
+    }
+
+    public Set<Contact> searchByName(String name) {
+        return contacts.stream().filter(c -> c.getName().equalsIgnoreCase(name)).collect(Collectors.toSet());
+    }
+
+    public void updatePhoneContact(String name, String number) {
+        Optional<Contact> aux = contacts.stream().filter(c -> c.getName().equalsIgnoreCase(name)).findFirst();
+        if (aux.isEmpty()) {
+            return;
+        }
+        for (Contact contact : contacts) {
+            if (aux.get().getName().equalsIgnoreCase(contact.getName())) {
+                contact.setPhoneNumber(number);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ContactAgenda contacts = new ContactAgenda();
+
+        contacts.addContact("Miguel", "+55(61)994304737");
+
+        contacts.showContacts();
+
+        System.out.println(contacts.searchByName("Miguel"));
+
+        contacts.updatePhoneContact("Miguel", "61981457250");
+        
+        contacts.showContacts();
+    }
+
+}
